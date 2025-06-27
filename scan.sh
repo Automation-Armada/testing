a99d7cff13da14e2ac667d75eb675a89c2f62c49
Thiago <149462281+thiagotobiasturk@users.noreply.github.com>
2025-06-27 04:07:00 -0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Escaneando archivo limpio..."
-clamscan  sample_file.txt
-
+ls
+clamscan  /sample_file.txt
 echo ""
 
 echo "Escaneando archivo con virus de prueba (EICAR)..."
-clamscan virus.txt
+clamscan /virus.txt
 
 echo ""
